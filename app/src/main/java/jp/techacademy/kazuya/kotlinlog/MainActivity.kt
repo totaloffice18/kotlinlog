cf04bd73e1b674ec771bc4aa6a1bea49d134b58d
totaloffice18 <total.office.18@gmail.com>
2024-08-15 12:37:10 +0900

    kotlinログ

---
+++ app/src/main/java/jp/techacademy/kazuya/kotlinlog/MainActivity.kt
@@ -0,0 +1,17 @@
+package jp.techacademy.kazuya.kotlinlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("kotlintest", "ログへの出力テスト")
+
+    }
+}
